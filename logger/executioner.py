c17226e0a5f492d8200369bfccb2790581a2fcdb
Manavalan Krishnan <manavalan.krishnan@sandisk.com>
2016-07-27 12:19:56 -0700

    multithreaded fio

--- logger/executioner.py
+++ logger/executioner.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python
 
-import threading
+from threading import Thread
 import time
 import paramiko
 import datetime
 import os
+import sys
 
+class FIOWorker(Thread):
+    def __init__(self, args, index):
+        Thread.__init__(self)
+        self.args = args
+        self.index = index
+
+    def run(self):
+        n = self.args["node"]
+        block_sz = self.args["block_sz"]
+        #index = self.args["index"]
+        index = self.index
+
+        if self.args["type"] =="prepare":
+            prepare_fio(n, block_sz, index)
+            return
+
+        if self.args["type"] =="execute":
+            load = self.args["load"]
+            time = self.args["time"]
+            log_path = self.args["log_path"]
+            num_fio = self.args["num_fio"]
+            execute_fio(n, load, block_sz, time, num_fio, log_path, index)
+        else:
+            print "Illegal type %s passed" %(self.args["type"])
+            sys.exit(-1)
+        
 def print_lines(lines):
     for l in lines:
         print l
@@ -19,7 +46,91 @@ def ssh_remote_run(client, script):
     sftp.put(script, destination)
     sftp.close()
 
-def execute_fio(n, load, block_sz, time, num_fio, log_path):
+def prepare_fio_worker(n, block_sz, num_files):
+    #ts = time()
+    threads = []
+
+    args = {}
+    args["type"] = "prepare"
+    args["node"] = n
+    args["block_sz"] = block_sz
+
+    #for i in [x for x in xrange(int(num_fio)) if x != 0]:
+    for i in range(int(num_fio)):
+        i = i + 1
+        args["index"] = str(i)
+        worker = FIOWorker(args, str(i))
+        threads += [worker]
+        worker.start()
+
+    for t in threads:
+        t.join()
+
+    #print('Took {}'.format(time() - ts))
+
+def execute_fio_worker(n, load, block_sz, time, num_fio, log_path):
+    #ts = time()
+    threads = []
+
+    args = {}
+    args["type"] = "execute"
+    args["node"] = n
+    args["load"] = load
+    args["block_sz"] = block_sz
+    args["time"] = time
+    args["log_path"] = log_path
+    args["num_fio"] = num_fio
+ 
+    for i in range(int(num_fio)):
+        i = i + 1
+        #for i in [x for x in xrange(int(num_fio)) if x != 0]:
+        args["index"] = str(i)
+        worker = FIOWorker(args, str(i))
+        threads += [worker]
+        worker.start()
+
+    for t in threads:
+        t.join()
+
+    #print('Took {}'.format(time() - ts))
+
+def clean_up_mount_point(n):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    ssh.load_system_host_keys()
+
+    ssh.connect(hostname=n, port=22, username='ems', password='ems123')
+
+    fio_script_path = "fio_cleaner.sh"
+
+    new_script_path = "/tmp/" + fio_script_path
+    sub_cmd = "chmod +x " + new_script_path + ";"
+ 
+    #cmd = sub_cmd + "bash -x "
+    cmd = sub_cmd + "bash "
+    cmd = cmd + new_script_path
+    print "Running fio cmd = " + cmd
+
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+
+    print "Execute cleanup stdout from node:%s" %(n)
+    print_lines(stderr.readlines())
+    print "cleanup stdout: ", stdout.readlines()
+    print_lines(stdout.readlines())
+
+    ssh.close()
+
+def copy_script(node, script_path):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    ssh.load_system_host_keys()
+
+    ssh.connect(hostname=node, port=22, username='ems', password='ems123')
+
+    ssh_remote_run(ssh, script_path)
+    ssh.close()
+
+def execute_fio(n, load, block_sz, time, num_fio, log_path, index):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
     ssh.load_system_host_keys()
@@ -27,14 +138,41 @@ def execute_fio(n, load, block_sz, time, num_fio, log_path):
     ssh.connect(hostname=n, port=22, username='ems', password='ems123')
 
     fio_script_path = "fio_runner.sh"
-    ssh_remote_run(ssh, fio_script_path)
 
     new_script_path = "/tmp/" + fio_script_path
     sub_cmd = "chmod +x " + new_script_path + ";"
  
-    cmd = sub_cmd + "bash -x "
+    #cmd = sub_cmd + "bash -x "
+    cmd = sub_cmd + "bash "
     cmd = cmd + new_script_path + " " + load + " " + block_sz + " " + \
-            time + " " + num_fio + " " + log_path
+            time + " " + num_fio + " " + log_path + " " + index
+    print "Running fio cmd = " + cmd
+
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+
+    print "Execute FIO stdout from node:%s" %(n)
+    print "fio stderr: "
+    print_lines(stderr.readlines())
+    print "fio stdout: ", stdout.readlines()
+    print_lines(stdout.readlines())
+
+    ssh.close()
+
+def prepare_fio(n, block_sz, index):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    ssh.load_system_host_keys()
+
+    ssh.connect(hostname=n, port=22, username='ems', password='ems123')
+
+    fio_script_path = "fio_prepare.sh"
+
+    new_script_path = "/tmp/" + fio_script_path
+    sub_cmd = "chmod +x " + new_script_path + ";"
+ 
+    cmd = sub_cmd + "bash "
+    cmd = cmd + new_script_path + " " + block_sz + " " + index
+
     print "Running fio cmd = " + cmd
 
     stdin, stdout, stderr = ssh.exec_command(cmd)
@@ -63,7 +201,7 @@ def execute_push_on_a_node(n, remote_script_path, args=None):
     else:
         cmd = new_remote_script_path
 
-    cmd = sub_cmd + "bash -x " + cmd
+    cmd = sub_cmd + "bash " + cmd
     print "Runing command:" + cmd
 
     stdin, stdout, stderr = ssh.exec_command(cmd)
@@ -119,28 +257,40 @@ def collect_all_stats(nodes, load, args):
         ssh.close()
 
 if __name__ == "__main__":
-    nodes = ["iflab1", "iflab2"]
-    client_node = "iflab10"
-    #load_profile = ["read", "write", "randrw"]
-    #block_sz = ["4k", "16k", "64k", "256k", "1024k", "4m", "16m"]
-    load_profile = ["read"]
-    block_sz = ["4k", "1m"]
-    time = "5"
-    num_fio = "1"
+    if sys.argv < 4:
+        print "python executioner.py <fio jobs> <num_job_per_fio> <duration>"
+        sys.exit(-1)
 
-    for load in load_profile:
-        for blk_sz in block_sz:
+    fio_jobs = sys.argv[1]
+    fio_count = sys.argv[2]
+    time = sys.argv[3]
+
+    nodes = ["iflab1", "iflab2", "iflab3", "iflab4", "iflab12"]
+    client_node = "iflab12"
+    load_profile = ["randread", "randwrite", "randrw"]
+    block_sz = ["4k", "64k", "1024k", "4m", "16m"]
+    #load_profile = ["read"]
+    #block_sz = ["4k", "1m"]
+
+    scripts = ["fio_prepare.sh", "fio_runner.sh", "fio_cleaner.sh"]
+    for script in scripts:
+        copy_script(client_node, script)
+
+    for blk_sz in block_sz:
+        clean_up_mount_point(client_node)
+        prepare_fio_worker(client_node, blk_sz, fio_jobs)
+
+        for load in load_profile:
             test_suffix_name = load + "_" + blk_sz
             log_path = create_log_path(test_suffix_name)
             args = []
             args.append(str(test_suffix_name))
             args.append(str(log_path))
             print "Prepared list" + str(args)
-            remote_collect_start(nodes, args[0])
 
             #
-            execute_fio(client_node, load, blk_sz, time, num_fio, args[1])
-
+            remote_collect_start(nodes, args[0])
+            execute_fio_worker(client_node, load, blk_sz, time, fio_jobs, args[1])
             #
             remote_collect_stop(nodes, args[0])
             collect_all_stats(nodes, load, args)
--- logger/fio_runner.sh
+++ logger/fio_runner.sh
@@ -1,7 +1,29 @@
-FIO=/home/ems/kanaujia/fio/fio
-MOUNT_NAME=/mnt/cephfs_perf/
+#FIO=/home/ems/kanaujia/fio/fioa
+FIO=fio
+#MOUNT_NAME=/mnt/cephfs_perf/
+MOUNT_NAME=/home/ems/kanaujia/dev-tools/logger/test_dir
+FILESIZE=1mb
+FILENAME=$MOUNT_NAME/$FILESIZE.blob
 
-for i in {1..$4}
-do
-    sudo $FIO --filename=$MOUNT_NAME/1tb.blob --name=$1.fio --output=$5/$2_$1.log --status-interval=5 --direct=1 --rw=$1 --bs=$2 --numjobs=8 --iodepth=8 --runtime=$3 --norandommap --time_based --ioengine=libaio --group_reporting --thread --filesize=1tb
-done
+if [ "$#" -ne 6 ]; then
+    echo "Illegal number of parameters"
+    echo "./fio_runner.sh <LOAD=randread> <Block SZ=4k> <Time=120> <# FIO=8> <LOG PATH> ><INDEX>"
+    exit -1
+fi
+
+LOAD=$1
+BLKSZ=$2
+TIME=$3
+FIO_COUNT=$4
+INDEX=$6
+OUTPUT=$5/$INDEX.fio.log
+
+echo "Staring FIO number $INDEX"
+sudo $FIO --filename=$FILENAME.$INDEX --name=benchmark.fio.$INDEX \
+--output=$OUTPUT --status-interval=5 --direct=1 --rw=$1 \
+--bs=$2 --numjobs=$FIO_COUNT --iodepth=8 --runtime=$3 \
+--size=$FILESIZE --randrepeat=0 --invalidate=1 \
+--time_based --ioengine=libaio \
+--group_reporting --thread --filesize=$FILESIZE --verify=0
+
+echo "fio runs completed"
