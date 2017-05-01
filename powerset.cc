9d71ced209364d44f4df7fbd483b228d5cfc7d2e
Vishal Kanaujia <vishalkanaujia@gmail.com>
2017-05-01 14:39:55 +0530

    Powerset of a set

---
+++ powerset.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int num;
+
+void printArray(int *array, int n)
+{
+    for (auto i = n - 1; i >= 0; i--) {
+        cout << array[i];
+    }
+    cout << endl;
+}
+
+void perm(int *arr, int k, int n, int v)
+{
+    if (0 >= n) {
+        printArray(arr, num);
+        return;
+    }
+
+    for (auto ii = v; ii <= k; ii++) { 
+        arr[n - 1] = ii;
+        perm(arr, k, n - 1, ii + 1);
+    }
+}
+
+
+int main()
+{
+    // Number of digits e.g. n=3 xxx, yyy
+    num = 3;
+
+    // Range of numbers e.g. (1...3)
+    int k = 3;
+    int *arr = (int*) malloc(sizeof(int) * num);
+    for (auto m = 1; m <= k; m++)
+        perm(arr, k, m, 1);
+}
